2a05a30e7b4e9c7d7559db57bb21ca16b0de170d
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2012-08-27 19:46:08 +0200

    Commit inicial. Crear app rails y editar .gitignore.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bagoaz::Application.config.secret_token = 'c1fce6edc7564e30df0a2363e858c90b486bc70d17b4af07da50a48409e28c7969c062f21bc834a233a298b539a4e56bdafc4850286ee99f5dc48d040dc4e53a'
