e2893e41314997b39f3f8175102e81fe1cedf95a
unknown <Ander@.(none)>
2012-09-05 19:51:35 +0200

    Realizado un cambio para que funcione el deploy a heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Bagoaz
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #Sin esta linea falla al hacer el deploy a heroku
+    config.assets.initialize_on_precompile = false
   end
 end
