7df294753265f3530149b23c1d4e3d9825cad9d3
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2012-11-25 09:49:41 +0100

    Sistema de registro, login, confirmación de email, recuperación de password implementado con devise y vistas adaptadas a bootstrap.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,11 +24,17 @@
 #
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, 
+         #:token_authenticatable,
+         #:omniauthable,
+         :confirmable,
+         :recoverable,
+         :registerable,
+         :rememberable, 
+         :trackable, 
+         #:timeoutable,
+         :validatable
+         #:lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
