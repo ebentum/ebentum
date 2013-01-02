e155296342c5a11154068aa0126f675dc5bcc601
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2013-01-02 10:43:03 +0100

    Configuración de envío de emails para producción.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,6 @@ Ebentum::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options   = { :host => 'localhost:3000' }
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -37,16 +33,18 @@ Ebentum::Application.configure do
   config.assets.debug = true
 
   # Envio de emails
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings   = {
-                                          :address              => "smtp.gmail.com",
-                                          :port                 => 587,
-                                          :domain               => 'localhost',
-                                          :user_name            => 'dev.ebentum',
-                                          :password             => 'jdaammtjd',
-                                          :authentication       => 'plain',
-                                          :enable_starttls_auto => true  
-                                          }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options   = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings         = {
+                                                :address              => "smtp.gmail.com",
+                                                :port                 => 587,
+                                                :domain               => 'localhost',
+                                                :user_name            => 'dev.ebentum',
+                                                :password             => 'jdaammtjd',
+                                                :authentication       => 'plain',
+                                                :enable_starttls_auto => true  
+                                                }
 
   config.paperclip_defaults = {
     :storage => :s3,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,20 @@ Ebentum::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # Envio de emails
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options   = { :host => 'www.ebentum.com' }
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.smtp_settings         = {
+                                                :address              => "smtp.gmail.com",
+                                                :port                 => 587,
+                                                :domain               => 'ebentum.com',
+                                                :user_name            => 'dev.ebentum',
+                                                :password             => 'jdaammtjd',
+                                                :authentication       => 'plain',
+                                                :enable_starttls_auto => true  
+                                                }
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
