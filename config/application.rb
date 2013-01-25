e438f28a05713b638d08f165ca04b82229da7c9f
Ander Mujika <andermujika@gmail.com>
2013-01-25 22:39:36 +0100

    Gema i18n-js

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module Ebentum
     config.assets.version = '1.0'
     
     #Sin esta linea falla al hacer el deploy a heroku
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
