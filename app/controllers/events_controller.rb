ae1e5f11e915121d0eda90c3a9e8d2f5221db693
Ander Mujika <andermujika@gmail.com>
2013-01-27 16:44:15 +0100

    Crear el esqueleto de los distintos listados

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,17 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+
+    #@events = current_user.followings.events
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @events }
+    end
+  end
+
+  def popular
+    @events = Event.all
     
     respond_to do |format|
       format.html # index.html.erb
