fccd857186dbd92359c53c1ecb6a2cefef00f6a9
tjd <txomin.jimenez@gmail.com>
2013-02-16 14:13:06 +0100

    Corregir formulario de edicion de usuario para utilizar devise correctamente.
    Actualmente obliga a introducir la contraseña para guardar los cambios, y a parte redirige a la pagina principal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,32 +55,32 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
+  # def edit
+  #   @user = User.find(params[:id])
 
-    @social_fb = @user.social_fb
+  #   @social_fb = @user.social_fb
 
-    if @user != current_user
-      respond_to do |format|
-        format.html  { render :action => "show" }
-      end
-    end
-  end
+  #   if @user != current_user
+  #     respond_to do |format|
+  #       format.html  { render :action => "show" }
+  #     end
+  #   end
+  # end
 
-  def update
-    @user = User.find(params[:id])
+  # def update
+  #   @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html  { redirect_to(@user,
-                      :notice => 'User was successfully updated.') }
-        format.json  { head :no_content }
-      else
-        format.html  { render :action => "edit" }
-        format.json  { render :json => @user.errors,
-                      :status => :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @user.update_attributes(params[:user])
+  #       format.html  { redirect_to(@user,
+  #                     :notice => 'User was successfully updated.') }
+  #       format.json  { head :no_content }
+  #     else
+  #       format.html  { render :action => "edit" }
+  #       format.json  { render :json => @user.errors,
+  #                     :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
 end
