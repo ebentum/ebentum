36bd9db25455639a5c5174b52002b31aa0c6c7eb
tjd <txomin.jimenez@gmail.com>
2013-03-17 16:37:25 +0100

    Modificar edit de usuarios para que no pida contraseña.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+
+
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+    end
+
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html  { redirect_to(@user,
+                      :notice => 'User was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @user.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
   def following
     @user = User.find(params[:id])
     @users = @user.all_followees
