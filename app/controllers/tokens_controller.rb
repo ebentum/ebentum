3124ea471880ed3542ab27653531bdf0fa9db02c
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2013-07-23 16:24:43 +0200

    Permitir crear tokens de acceso sin estar logueado.

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,5 +1,7 @@
 class TokensController < ApplicationController
 
+  skip_before_filter :auth_user
+
   def create
     user = User.find(params[:user_id])
     if params[:provider] == 'facebook'
@@ -7,7 +9,7 @@ class TokensController < ApplicationController
       user.fbtoken = token
     elsif params[:provider] == 'twitter'
       token = Twtoken.new(params[:token])
-      user.twtoken = token  
+      user.twtoken = token
     end
     render :json => user.save
   end
@@ -17,13 +19,13 @@ class TokensController < ApplicationController
       if current_user.fbtoken
         render :json => current_user.fbtoken.update_attributes(:autopublish => !current_user.fbtoken.autopublish)
       else
-        render :json => true 
+        render :json => true
       end
     elsif params[:provider] == 'twitter'
       if current_user.twtoken
-        render :json => current_user.twtoken.update_attributes(:autopublish => !current_user.twtoken.autopublish)  
+        render :json => current_user.twtoken.update_attributes(:autopublish => !current_user.twtoken.autopublish)
       else
-        render :json => true 
+        render :json => true
       end
     end
   end
@@ -32,7 +34,7 @@ class TokensController < ApplicationController
     if params[:provider] == 'facebook'
       current_user.fbtoken = nil
     elsif params[:provider] == 'twitter'
-      current_user.twtoken = nil  
+      current_user.twtoken = nil
     end
     render :json => true
   end
