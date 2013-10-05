23393ae35b1a319c6f0d373d5241c04c16fff92e
Ander Mujika <andermujika@gmail.com>
2013-10-05 16:28:08 +0200

    En caso de que el evento no tenga una imagen que no falle

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -10,16 +10,23 @@ module ActivitiesHelper
     activity.actor.url = Rails.application.routes.url_helpers.user_path(actor) 
     activity.actor.objectType = "user"
     activity.actor.displayName = actor.complete_name
-    activity.actor.photoUrl = actor.main_picture.photo.url(:thumb) rescue "/photos/thumb/missing.png"
+
+    if actor.main_picture.photo?
+      activity.actor.photoUrl = actor.main_picture.photo.url(:thumb)
+    else
+      activity.actor.photoUrl = "/photos/thumb/missing.png"
+    end
 
     activity.subject = Subject.new
     activity.subject._id = subject._id
     activity.subject.objectType = "event"
     activity.subject.url = Rails.application.routes.url_helpers.event_path(subject)
     activity.subject.displayName = subject.name
-    activity.subject.photoUrl = subject.main_picture.photo.url(:card)
-    activity.subject.photoWidth = subject.main_picture.photo_dimensions["card"][0]
-    activity.subject.photoHeight = subject.main_picture.photo_dimensions["card"][1]
+    if subject.main_picture.photo?
+      activity.subject.photoUrl = subject.main_picture.photo.url(:card)
+      activity.subject.photoWidth = subject.main_picture.photo_dimensions["card"][0]
+      activity.subject.photoHeight = subject.main_picture.photo_dimensions["card"][1]
+    end
     activity.subject.address = subject.place
     activity.subject.startDate = subject.start_date
     activity.subject.startTime = subject.start_time
