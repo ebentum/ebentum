e091491b208f6fa5aa57e8434cd781a39458f82d
Ander Mujika <andermujika@gmail.com>
2013-11-23 16:56:29 +0100

    Cambios para que funcione en beanstalk

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby '1.9.3'
-
 gem 'rails', '~> 3.2.12'
 
 # Gems used only for assets and not required
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Ebentum::Application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_KEY']
     },
     :s3_host_name => 's3-eu-west-1.amazonaws.com'
   }
