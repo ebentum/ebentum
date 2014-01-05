007c8cd8c5aeca5f8093ada5cc882621415c419d
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2014-01-05 17:11:35 +0100

    Quitar errores 500 para solucionar errores del scraper de Facebook.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -74,7 +74,11 @@ class ApplicationController < ActionController::Base
   private
 
   def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    if request.env['HTTP_ACCEPT_LANGUAGE'] == nil
+      'es' # TODO: Para arreglar el error 500 del scraper. Hay que definir default.
+    else
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
   end
 
 end
