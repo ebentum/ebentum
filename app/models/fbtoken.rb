f2948bcc6642046393920f80315abab1c23e2dcb
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2014-05-18 12:20:01 +0200

    Pruebas para arreglar el error tras crear el fbtoken.

--- app/models/fbtoken.rb
+++ app/models/fbtoken.rb
@@ -11,9 +11,9 @@ class Fbtoken
 
   embedded_in :user
 
-  def get_token_friends_with_ebentum(current_user)
+  def get_token_friends_with_ebentum
     # instanciamos el objeto fb
-    fb = Koala::Facebook::API.new(current_user.fbtoken.token)
+    fb = Koala::Facebook::API.new(self.token)
     # uids de fb de nuestros amigos de fb que tienen ebentum
     fb_uids = fb.get_connections("me", "friends", :fields => "id, installed") # amigos de fb. obtenemos id e installed (ebentum)
               .select {|user| user.key?('installed')} # nos quedamos con los que tienen ebentum instalado
@@ -27,7 +27,7 @@ class Fbtoken
   after_create do |fbtoken|
     # obtenemos la lista de usuarios que van a recibir la actividad
     # amigos de fb del usuario que tienen ebentum y que no seguimos en ebentum
-    receivers = get_token_friends_with_ebentum(self.user).map{|fbfriend| fbfriend if !current_user.follower_of?(fbfriend)}
+    receivers = get_token_friends_with_ebentum#.map{|fbfriend| fbfriend if !current_user.follower_of?(fbfriend)}
     # sacamos las ids
     receiver_ids = receivers.map{|r| r.id}
 
