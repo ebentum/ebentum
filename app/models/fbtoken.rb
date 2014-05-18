a06e7e991b17ede80b84294dba14d454a2ad3be6
Jordi Díaz Añorga <diaz.jordi@gmail.com>
2014-05-18 13:10:00 +0200

    Pruebas para arreglar el error tras crear el fbtoken.

--- app/models/fbtoken.rb
+++ app/models/fbtoken.rb
@@ -21,13 +21,17 @@ class Fbtoken
     # obtenemos los usuarios de ebentum que tienen esas uids de en el token de fb
     logger.info('fb_uids en fbtoken.rb')
     logger.info(fb_uids)
-    User.where('fbtoken.uid' => fb_uids)
+    users = User.where('fbtoken.uid' => fb_uids)
+    users
   end
 
   after_create do |fbtoken|
     # obtenemos la lista de usuarios que van a recibir la actividad
-    # amigos de fb del usuario que tienen ebentum y que no seguimos en ebentum
+    # amigos de fb del usuario que tienen ebentum #y que no seguimos en ebentum
     receivers = get_token_friends_with_ebentum#.map{|fbfriend| fbfriend if !current_user.follower_of?(fbfriend)}
+    logger.info('receivers en fbtoken.rb')
+    logger.info(receivers)
+
     # sacamos las ids
     receiver_ids = receivers.map{|r| r.id}
 
